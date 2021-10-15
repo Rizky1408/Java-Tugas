3823beadb566cccd53631285efddde2ba17d89aa
Rizky1408 <agungdwisatria23@gmail.com>
2021-10-15 13:34:58 +0800

    uploud

---
+++ src/Rizky.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Rizky {
+    public static void main(String[] args){
+        String Nama;
+
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.print("Masukan Nama :");
+        Nama = Keyboard.nextLine();
+        System.out.println("Welcome to Java Programing " + Nama);
+    }
+}
---
+++ src/TheTree.java
@@ -0,0 +1,5 @@
+public class TheTree {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I think that I shall never see,\na poem as lovely as a tree.\na Tree whose hungry mouth is pressed\nAgainst the Earth's sweet flowing breast.");
+    }
+}
